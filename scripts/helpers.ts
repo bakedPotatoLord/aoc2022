4e6593e14f8cebb895e1fcfe687d74739e03c217
= <918780@dpsk12.net>
2022-12-01 10:11:50 -0700

    added split criteria param

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,11 +1,11 @@
 import fs from 'fs/promises';
 
-export  async function parseInput(name:string):Promise<string[]>{
-  return (await fs.readFile(`./inputs/${name}`,"utf-8")).split('\n')
+export  async function parseInput(name:string,splitCriteria:string|RegExp):Promise<string[]>{
+  return (await fs.readFile(`./inputs/${name}`,"utf-8")).split(splitCriteria)
 }
 
-export  async function parseInputAsNum(name:string):Promise<number[]>{
-  return Array.from((await fs.readFile(`./inputs/${name}`,"utf-8")).split('\n'),(v)=>parseFloat(v))
+export  async function parseInputAsNum(name:string,splitCriteria:string|RegExp):Promise<number[]>{
+  return Array.from((await fs.readFile(`./inputs/${name}`,"utf-8")).split(splitCriteria),(v)=>parseFloat(v))
 }
 
 export function arrSum(arr:number[]){
