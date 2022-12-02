5a456454731eb050d584afd52e8856de9c1c4a08
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-12-02 16:41:11 +0000

    added sort formula

--- scripts/day1.ts
+++ scripts/day1.ts
@@ -4,7 +4,6 @@ let input2 = await parseInputAsNum("1.txt","\n")
 
 let calArray:number[] = []
 let totalArray:number[] = []
-let topElves:number[]= []
 
 for(let i of input2){
   if(!isNaN(i)){
@@ -16,10 +15,5 @@ for(let i of input2){
 }
 console.log("solution1: ",Math.max(...totalArray))
 
-
-for(let i = 0;i<3;i++){
-	let j = totalArray.indexOf(Math.max(...totalArray))
-	topElves.push(totalArray[j])
-	totalArray.splice(j,1,0)
-}
-console.log("solution2: ",arrSum(topElves))
+totalArray.sort((a, b) =>a - b);
+console.log("solution2: ",arrSum(totalArray.slice(-3)))
