6779cd3ec40ecfd0c0d021502897ef7904b99f8e
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-12-02 21:18:55 +0000

    add comments

--- scripts/day1.ts
+++ scripts/day1.ts
@@ -1,9 +1,8 @@
 import { parseInput,arrSum} from "./helpers.js";
-
+//parse input into sums of calorie groups
 let input2 = await parseInput("1.txt","\n\n")
-
 let data = input2.map((el)=> arrSum(el.split("\n").map(el=>parseInt(el))))
-
+//print outputs. 
 console.log("solution1: ",Math.max(...data))
 data.sort((a, b) =>a - b);
-console.log("solution2: ",arrSum(data.slice(-3)))
+console.log("solution2: ",arrSum(data.slice(-3)));
