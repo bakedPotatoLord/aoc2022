675d9b82d1cd05565fb82fec1a9bca494a5c0346
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-12-03 17:26:15 +0000

    added comments and stylistic changes

--- scripts/day3.ts
+++ scripts/day3.ts
@@ -2,34 +2,39 @@ import { parseInput,arrSum} from "./helpers.js";
 //split into a 2d string array
 let input = await parseInput("3.txt","\n")
 
-
+//split into 3d string array
 let parsed = input.map(el=>{
   let arr = Array.from(el)
   return [arr.slice(0,arr.length/2),arr.slice(arr.length/2,arr.length)]
 })
-function getThing(x:string){
+
+//declare getChar function
+//it could be an LUT, but this is less work
+function getCharVal(x:string){
   if(x != x.toUpperCase()){
     return x.charCodeAt(0)-96
   }else{
-    return x.charCodeAt(0)-64+26
+    return x.charCodeAt(0)-64+26 //dont ask questions
   }
 }
+//filter for double included elements
+let charVals = parsed.map(el=> el[0].filter(element => el[1].includes(element)) )
+//get char vals, turning string[][] into number[]
+.map(el=>getCharVal(el[0]))
 
-let sameChar = parsed.map(el=>{
-  return el[0].filter(element => el[1].includes(element));
-})
-console.log("p1: ", arrSum(sameChar.map(el=>getThing(el[0]))))
+console.log("p1: ", arrSum(charVals))
 
+// create groups array
 let groups: string[][][] = []
-
-while(input.length !=0){
+// 
+//theres probably a better way to do this but whatevs
+while(input.length /*because 0 is falsy 😏*/){
   groups.push(input.slice(0,3).map(el=>Array.from(el)))
   input.splice(0,3)
 }
+//filter for elems repeated across all 3 arrays
+let charVals2 = groups.map(el=>el[0].filter(element => el[1].includes(element) && el[2].includes(element)))
+//map to number[]
+.map(el=>getCharVal(el[0]))
 
-let sameChar2 = groups.map(el=>{
-  return el[0].filter(element => el[1].includes(element) && el[2].includes(element));
-})
-.map(el=>getThing(el[0]))
-
-console.log("p2: ",arrSum(sameChar2))
+console.log("p2: ",arrSum(charVals2))
