3a9e77817a3d5088412082f2ed8606166e0a6ea8
bakedpotatolord <918780@dpsk12.net>
2022-12-05 10:42:16 -0700

    add sloppy part1 solution

--- scripts/day5.ts
+++ scripts/day5.ts
@@ -1,15 +1,40 @@
 import { parseInput,arrSum} from "./helpers.js";
 //split into a 2d string array
-let boxes = (await parseInput("5.txt","\n\n"))[0]
-.split("\n").slice(0,-1)
-.map(el=>(el.matchAll(/[(.*?)]/g)
+let input = (await parseInput("5.txt","\r\n\r\n"))
+let boxes = input[0]
+.split("\r\n").slice(0,-1)
+
+let parsedBoxes: string[][] = [
+boxes.map(el=>el.charAt(1)),
+boxes.map(el=>el.charAt(5)),
+boxes.map(el=>el.charAt(9)),
+boxes.map(el=>el.charAt(13)),
+boxes.map(el=>el.charAt(17)),
+boxes.map(el=>el.charAt(21)),
+boxes.map(el=>el.charAt(25)),
+boxes.map(el=>el.charAt(29)),
+boxes.map(el=>el.charAt(33)),
+]
+.map(el=>el.reverse())
+.map(el=>el.filter(el=>(el != " ")))
+
+console.log(parsedBoxes)
+
+let instructions = input[1]
+.split("\r\n")
+.map(el=>el.split(/move | from | to /).slice(-3).map(
+  el=>parseInt(el)
 ))
 
-console.log(boxes[0])
 
-let instructions = (await parseInput("5.txt","\n\n"))[1]
-.split("\n")
-.map(el=>el.split(/move | from | to /).slice(-3))
+console.log(instructions)
 
+instructions.forEach(i=>{
+  let ins = 0
+  for(ins =0;ins<i[0];ins++){
+    parsedBoxes[i[2]-1].push(parsedBoxes[i[1]-1].pop())
+  }
+  
+})
 
-//console.log(instructions)
+console.log("p1: " ,parsedBoxes.map(el=>el.pop()))
