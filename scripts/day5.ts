c13939b66975dd12e7280521ffbf68bd5a3ae9db
bakedpotatolord <918780@dpsk12.net>
2022-12-05 11:21:06 -0700

    add day2 solution

--- scripts/day5.ts
+++ scripts/day5.ts
@@ -4,7 +4,7 @@ let input = (await parseInput("5.txt","\r\n\r\n"))
 let boxes = input[0]
 .split("\r\n").slice(0,-1)
 
-let parsedBoxes: string[][] = [
+let parsedBoxes: string[][]=[
 boxes.map(el=>el.charAt(1)),
 boxes.map(el=>el.charAt(5)),
 boxes.map(el=>el.charAt(9)),
@@ -18,7 +18,7 @@ boxes.map(el=>el.charAt(33)),
 .map(el=>el.reverse())
 .map(el=>el.filter(el=>(el != " ")))
 
-console.log(parsedBoxes)
+let parsedBoxClone = JSON.parse(JSON.stringify(parsedBoxes))
 
 let instructions = input[1]
 .split("\r\n")
@@ -26,15 +26,15 @@ let instructions = input[1]
   el=>parseInt(el)
 ))
 
-
-console.log(instructions)
-
 instructions.forEach(i=>{
-  let ins = 0
-  for(ins =0;ins<i[0];ins++){
+  for(let ins =0;ins<i[0];ins++){
     parsedBoxes[i[2]-1].push(parsedBoxes[i[1]-1].pop())
   }
-  
 })
-
 console.log("p1: " ,parsedBoxes.map(el=>el.pop()))
+
+instructions.forEach(i=>{
+  parsedBoxClone[i[2]-1].push(...parsedBoxClone[i[1]-1].splice(-i[0]))
+})
+console.log("p2: " ,parsedBoxClone.map(el=>el.pop()))
+
