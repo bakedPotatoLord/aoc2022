1fc04b192ed7114d0f28afa13df5745c20c3ad6c
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-12-07 14:46:28 +0000

    quicksave

---
+++ scripts/day7.ts
@@ -0,0 +1,26 @@
+import { parseInput,arrSum,hasDuplicates} from "./helpers.js";
+//split into a 2d string array
+let input = (await parseInput("y.txt","\n"))
+.map(el=>el.split(" ")
+.map(el=>{
+  if(el){
+
+  }
+}))
+
+
+let directory:any[] = []
+
+class Node{
+  children: Node[]
+  values:number[]
+  constructor(){
+    this.children = []
+    this.values = []
+  }
+
+  addChild(child:Node|number){
+
+  }
+}
+
