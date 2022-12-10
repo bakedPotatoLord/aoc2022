94b84a5a15a2b1246c6ad3ff94f787c150256d0e
= <918780@dpsk12.net>
2022-12-10 15:31:35 -0700

    completed day 8 solution

--- scripts/day8.ts
+++ scripts/day8.ts
@@ -4,7 +4,6 @@ let input = (await parseInput("8.txt","\r\n"))
 .map(el=>el.split("")
 .map(el=>parseInt(el)))
 
-
 //i: top to bottom
 //j: left to right
 let visible = input.map((el,i)=>
@@ -25,35 +24,26 @@ console.log("p1: ",sol1)
 let scenicScore = input.map((el,i)=>
   el.map((e,j)=>{ //check if visible
     if(i == 0 || i == input.length-1 || j== 0 || j ==el.length-1) return 0
-    console.log(i,j)
-    console.log(
-      (visibleTrees(deepCopy(el).splice(0,j),e)) , //looking from left
-    (visibleTrees(deepCopy(el).splice(j+1,el.length),e)), //looking from right
-    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(0,i),e)) , //looking from top
-    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(i+1,el.length),e))
-    )
-    return (visibleTrees(deepCopy(el).splice(0,j),e)) * //looking left
+    
+    return (visibleTrees(deepCopy(el).splice(0,j).reverse(),e)) * //looking left
     (visibleTrees(deepCopy(el).splice(j+1,el.length),e))* //looking right
-    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(0,i),e)) * //looking from top
-    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(i+1,el.length),e))//looking from bottom
+    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(0,i).reverse(),e)) * //looking up
+    (visibleTrees(deepCopy(input.map(el=>el[j])).splice(i+1,el.length),e))//looking down
 
   })
 )
-
-console.log("p2","  ")
+console.log("p2",Math.max(...scenicScore.map(el=>Math.max(...el))))
 
 
 function lowerThan(arr:number[],num:number){
   return arr.filter(el=>el<num).length == arr.length
 }
-
 function visibleTrees(arr:number[],num:number){
   let ar:number[] = deepCopy(arr)
-  let i = 0
-  while(ar[0] < num && ar.length != 0){
+  let i = 1
+  while(ar[0] < num && ar.length > 1){
     ar.shift()
     i++
   }
-  return i==0 ? 1 :i
-
+  return i
 }
