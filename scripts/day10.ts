74e3cd1772cf6eff8e9fd40a11022fc464cb8df3
= <918780@dpsk12.net>
2022-12-11 21:01:07 -0700

    add part 1 solution

---
+++ scripts/day10.ts
@@ -0,0 +1,41 @@
+import { parseInput,arrSum, deepCopy,Point} from "./helpers.js";
+
+//split into instruction array 🤏
+let input = (await parseInput("10.txt","\r\n"))
+.map(el=>el.split(" "))
+.map(el=>[el[0],el[1]])
+
+let strength = 1
+
+let vals:number[] = []
+
+input.forEach((instr)=>{
+  if(instr[0] == "addx"){
+    vals.push(strength)
+    vals.push(strength)
+    strength += parseInt(instr[1])
+  }else if(instr[0]== "noop"){
+    vals.push(strength)
+  }
+  
+})
+
+vals.unshift(0)
+
+let strengthSums = arrSum(vals.map((el,i)=>{
+  if((i+20) %40 == 0) console.log(el*i)
+   return (i+20) %40 == 0 ? el*i :0
+  }))
+
+console.log("p1:",strengthSums)
+
+let sprite = 1
+vals.shift()
+
+for(let i of vals){
+
+}
+
+let screen:string[] = []
+
+console.log(screen)
