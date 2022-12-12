b8b7222b5bd1ea127eb787bd9350048aea107e42
= <918780@dpsk12.net>
2022-12-11 21:32:08 -0700

    clean it up and add comments

--- scripts/day10.ts
+++ scripts/day10.ts
@@ -6,42 +6,28 @@ let input = (await parseInput("10.txt","\r\n"))
 .map(el=>[el[0],el[1]])
 
 let strength = 1
-
 let vals:number[] = []
 
 input.forEach((instr)=>{
   if(instr[0] == "addx"){
+    //indexes represent cycles
     vals.push(strength)
     vals.push(strength)
     strength += parseInt(instr[1])
   }else if(instr[0]== "noop"){
     vals.push(strength)
   }
-  
 })
-
-vals.unshift(0)
-
-let strengthSums = arrSum(vals.map((el,i)=>{
-  if((i+20) %40 == 0) console.log(el*i)
-   return (i+20) %40 == 0 ? el*i :0
-  }))
+vals.unshift(0) // idk why this fixes it but it does
+let strengthSums = arrSum(vals.map((el,i)=>(i+20) %40 == 0 ? el*i :0))
 
 console.log("p1:",strengthSums)
 
-let sprite = 1
-vals.shift()
+vals.shift() //shift back into place
 let screen = ""
 
 vals.forEach((val,i)=>{
-  if(i == 38){
-    console.log("potato")
-  }
-  if(Math.abs((i%40)-val) <=1){
-    screen+="#"
-  }else{
-    screen+="."
-  }
+  screen += (Math.abs((i%40)-val) <=1 ? "#":".") //update screen
 })
-
+//split into screen format
 console.log(screen.match(/.{1,40}/g).join("\n"))
