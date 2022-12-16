74ef0cd2bbf92cc97db02097048c4642750e43a1
= <918780@dpsk12.net>
2022-12-15 21:07:41 -0700

    complete part one

--- scripts/day15.ts
+++ scripts/day15.ts
@@ -6,12 +6,26 @@ let input = (await parseInput("15.txt","\r\n"))
   .map(el=>parseInt(el))
 )
 
+let row:boolean[] = new Array(16_000_000).fill(false)
+const offset = 8_000_000
+const line = 2_000_000
 
-let row:boolean[] = new Array(100).fill(false)
+const taxiDist = (p1:Point,p2:Point)=> Math.abs(p1.x-p2.x)+Math.abs(p1.y-p2.y)
+
+input.forEach(el=>{
+  let sensor = new Point(el[0],el[1])
+  let beacon = new Point(el[2],el[3])
+  let dist = taxiDist(sensor,beacon)
+  let xdist = dist - Math.abs(sensor.y-line)
+
+  if(xdist >= 0){
+    row.fill(true,sensor.x-xdist+offset,sensor.x+xdist+1+offset)
+  }
+  if(beacon.y == line){
+    row[beacon.x+offset] = false
+  }
+})
 
 
-console.log('p1')
-
-
-
+console.log('p1',row.filter(el=>el).length)
 
