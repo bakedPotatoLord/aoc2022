899448cc99f6fa5e927f688540bd9115f47e533e
bakedpotatolord <918780@dpsk12.net>
2022-12-18 21:01:35 -0700

    seperate part 1 and 2 solution

--- scripts/day6.ts
+++ scripts/day6.ts
@@ -2,13 +2,23 @@ import { parseInput,arrSum,hasDuplicates} from "./helpers.js";
 //split into a 2d string array
 let input = (await parseInput("6.txt",""))
 
-console.log("starting")
+
+for(let i in input){
+  let k = parseInt(i)
+  if(k > 3){
+    if(!hasDuplicates(input.slice(k-4,k))  ){
+      console.log("p1: ",i)
+      break
+    }
+  }
+}
+
 
 for(let i in input){
   let k = parseInt(i)
   if(k > 13){
     if(!hasDuplicates(input.slice(k-14,k))  ){
-      console.log("p1: ",i)
+      console.log("p2: ",i)
       break
     }
   }
