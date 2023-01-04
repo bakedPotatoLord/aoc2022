e9b2f2514cbceac6e963cb329b24ed067d45eb75
= <918780@dpsk12.net>
2023-01-04 16:26:53 -0700

    add solution

--- scripts/day18.ts
+++ scripts/day18.ts
@@ -15,7 +15,6 @@ function merge(a1:number[],a2:number[]){
   return a1.map((el,i)=>el-a2[i])
 }
 
-/*
 input.forEach((in1,i)=>{
   input.forEach(in2=>{
     if(Math.hypot(...merge(in1.arr,in2.arr)) == 1){
@@ -25,7 +24,6 @@ input.forEach((in1,i)=>{
 })
 
 console.log("p1",arrSum(input.map(el=>el.size)))
-*/
 
 let surfaceCounter =  0
 
@@ -34,24 +32,24 @@ interface cubeData{
   visited:boolean
 }
 
-let box:cubeData[][][] = Array(23).fill(undefined).map(()=>
-  Array(23).fill(undefined).map(()=>
-    Array(23).fill(undefined).map(()=>{return{solid:false,visited:false}})
+let box:cubeData[][][] = Array(25).fill(undefined).map(()=>
+  Array(25).fill(undefined).map(()=>
+    Array(25).fill(undefined).map(()=>{return{solid:false,visited:false}})
   )
 )
 
-
 input.forEach(el=>{
-  box[el.arr[0]+1][el.arr[1]+1][el.arr[2]+1].solid = true
+  box[el.arr[0]+2][el.arr[1]+2][el.arr[2]+2].solid = true
 })
 
 type v3 = [number,number,number]
 
-function isOutsideRange(x:number,y:number,z:number){
-  return x <0 || x >22 ||
-    y < 0 || y >22 ||
-    z < 0 || z >22
-    
+function isInsideRange(x:number,y:number,z:number){
+  return (
+    x >= 0 && x < 25 &&
+    y >= 0 && y < 25 &&
+    z >= 0 && z < 25
+  )
 }
 
 const touching=(x:number,y:number,z:number):v3[]=>
@@ -63,13 +61,13 @@ const touching=(x:number,y:number,z:number):v3[]=>
     [x,y,z+1],
     [x,y,z-1],
   ])
-  .filter(el=>!isOutsideRange(...el))
+  .filter(el=>isInsideRange(...el))
 
+//start que
 let que:v3[] = [[0,0,0]]
-
+//expand over cube within boundries
 while(que.length > 0 ){
   let val = que.shift()
-
   touching(...val).forEach(el=>{
     let cube = box[el[0]][el[1]][el[2]]
     if(cube.solid){
@@ -81,9 +79,7 @@ while(que.length > 0 ){
       }
     }
   })
-
 }
 
-// > 2501
 console.log('p2: ',surfaceCounter)
 
