102f38ee3b279713bc2a2cd5b80f47d8e4c3b666
bakedpotatolord <918780@dpsk12.net>
2023-01-06 23:21:29 -0700

    quicksave bc im tired

--- scripts/day23.ts
+++ scripts/day23.ts
@@ -11,9 +11,17 @@ class Elf extends Point{
     let xy = this.proposed.split(',').map(el=>parseInt(el))
     this.x = xy[0]
     this.y = xy[1]
-    elves.delete(toIntHash(this.x,this.y))
-    elves.set(this.proposed,this)
+  }
 
+  hasNeigbors(){
+    return elfPositions.has(toIntHash(this.x-1,this.y)) ||
+    elfPositions.has(toIntHash(this.x-1,this.y+1)) ||
+    elfPositions.has(toIntHash(this.x-1,this.y-1)) ||
+    elfPositions.has(toIntHash(this.x+1,this.y)) ||
+    elfPositions.has(toIntHash(this.x+1,this.y+1)) ||
+    elfPositions.has(toIntHash(this.x+1,this.y-1)) ||
+    elfPositions.has(toIntHash(this.x,this.y+1)) ||
+    elfPositions.has(toIntHash(this.x,this.y-1)) 
   }
 }
 
@@ -33,39 +41,105 @@ function toIntHash(a:number|bigint,b:number|bigint){
 
 let elves:Elf[] = []
 
-let considered:boolean[][] = Array(6)
+let elfPositions:Set<string> = new Set()
+
+let considered:Set<bigint|string> = new Set()
 
 let duplicates:Set<bigint|string> = new Set()
 
 input.forEach((ln,y)=>{
   ln.forEach((el,x)=>{
-    if(el) elves.push(new Elf(x,y))
+    if(el){
+      elves.push(new Elf(x,y))
+      elfPositions.add(x+','+y)
+    } 
   })
 })
 
-elves.forEach(el=>{
-  if(!(elves.has(toIntHash(el.x,el.y-1)) ||elves.has(toIntHash(el.x+1,el.y-1)) || elves.has(toIntHash(el.x-1,el.y-1)))){
-    //north
-    el.proposed = toIntHash(el.x,el.y-1)
-    if(considered.has(el.proposed)) duplicates.add(el.proposed)
-    considered.add(el.proposed)
-  }else if(!(elves.has(toIntHash(el.x,el.y+1)) ||elves.has(toIntHash(el.x+1,el.y+1)) || elves.has(toIntHash(el.x-1,el.y+1)))){
-    //south
-    el.proposed = toIntHash(el.x,el.y+1)
-    if(considered.has(el.proposed)) duplicates.add(el.proposed)
-    considered.add(el.proposed)
-  }else if(!(elves.has(toIntHash(el.x-1,el.y)) ||elves.has(toIntHash(el.x-1,el.y+1)) || elves.has(toIntHash(el.x-1,el.y-1)))){
-    //West
-    el.proposed = toIntHash(el.x-1,el.y)
-    if(considered.has(el.proposed)) duplicates.add(el.proposed)
-    considered.add(el.proposed)
-  }else if(!(elves.has(toIntHash(el.x+1,el.y)) ||elves.has(toIntHash(el.x+1,el.y+1)) || elves.has(toIntHash(el.x+1,el.y-1)))){
-    //East
-    el.proposed = toIntHash(el.x+1,el.y)
-    if(considered.has(el.proposed)) duplicates.add(el.proposed)
-    considered.add(el.proposed)
+let checks=[
+  (el:Elf)=>{
+    if(!(elfPositions.has(toIntHash(el.x,el.y-1)) ||elfPositions.has(toIntHash(el.x+1,el.y-1)) || elfPositions.has(toIntHash(el.x-1,el.y-1)))){
+      //north
+      el.proposed = toIntHash(el.x,el.y-1)
+      if(considered.has(el.proposed)) duplicates.add(el.proposed)
+      considered.add(el.proposed)
+      return true
+    }else{
+      return false
+    }
+  },
+  (el:Elf)=>{
+    if(!(elfPositions.has(toIntHash(el.x,el.y+1)) ||elfPositions.has(toIntHash(el.x+1,el.y+1)) || elfPositions.has(toIntHash(el.x-1,el.y+1)))){
+      //south
+      el.proposed = toIntHash(el.x,el.y+1)
+      if(considered.has(el.proposed)) duplicates.add(el.proposed)
+      considered.add(el.proposed)
+      return true
+    }else{
+      return false
+    }
+  },
+  (el:Elf)=>{
+    if(!(elfPositions.has(toIntHash(el.x-1,el.y)) ||elfPositions.has(toIntHash(el.x-1,el.y+1)) || elfPositions.has(toIntHash(el.x-1,el.y-1)))){
+      //West
+      el.proposed = toIntHash(el.x-1,el.y)
+      if(considered.has(el.proposed)) duplicates.add(el.proposed)
+      considered.add(el.proposed)
+      return true
+    }else{
+      return false
+    }
+  },
+  (el:Elf)=>{
+    if(!(elfPositions.has(toIntHash(el.x+1,el.y)) ||elfPositions.has(toIntHash(el.x+1,el.y+1)) || elfPositions.has(toIntHash(el.x+1,el.y-1)))){
+      //East
+      el.proposed = toIntHash(el.x+1,el.y)
+      if(considered.has(el.proposed)) duplicates.add(el.proposed)
+      considered.add(el.proposed)
+      return true
+    }else{
+      return false
+    }
   }
-})
+  
+]
+const numRounds = 10
 
+for(let round = 0;round<numRounds;round++ ){
+  
 
-console.log("p1:")
+  elves.forEach(el=>{
+
+    if(el.hasNeigbors()){
+      check:{
+        for(let i of checks){
+          if(i(el)) break check
+        }
+        el.proposed = el.toIntHash()
+      }
+    }else{
+      el.proposed = el.toIntHash()
+    }
+  })
+  
+  elves.forEach(el=>{
+
+      if(!duplicates.has(el.proposed)){
+        elfPositions.delete(el.toIntHash())
+        el.goToProposed()
+        elfPositions.add(el.proposed)
+      }
+
+  })
+  checks.push(checks.shift())
+  duplicates.clear()
+  considered.clear()
+  console.log()
+}
+
+let height = Math.max(...elves.map(e=>e.y)) -Math.min(...elves.map(e=>e.y))
+let width = Math.max(...elves.map(e=>e.x)) - Math.min(...elves.map(e=>e.x))
+
+let totalSquares = (height * width)
+
+console.log("p1:",totalSquares-elves.length)
