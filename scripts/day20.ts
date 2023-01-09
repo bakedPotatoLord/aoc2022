4215e108800a36bd980452e48537c5577f7edbb5
= <918780@dpsk12.net>
2023-01-08 18:36:21 -0700

    make it more self-explanitory

--- scripts/day20.ts
+++ scripts/day20.ts
@@ -5,19 +5,59 @@ let input= (await parseInput("20.txt","\r\n"))
 
 let ogInput = deepCopy(input)
 
-ogInput.forEach((el,i)=>{
-  let ind= input.findIndex((e)=>e==el)
-  let instertInd = ((ind+ el )% (input.length ))
-  if(instertInd == 0) instertInd = input.length
+let len = input.length
+
+//console.log(input.join(', '))
+
+function getInsertInd(selfIndex:number,val:number){
+  let moves = Math.abs(val)
+  let insertInd =selfIndex
+
+  while(moves > 0){
+    if(val > 0){
+      //if positive
+      if(insertInd == len -1){
+        insertInd = 1
+      }else{
+        insertInd++
+      }
+    }else{
+      //if negative
+      if(insertInd == 0){
+        insertInd = len- 2
+      }else if(insertInd == 1){
+        insertInd = len -1
+      }else{
+        insertInd--
+      }
+
+    }
+    moves --
+  }
+
+  return insertInd
+}
+
+ogInput.forEach((el)=>{
+  let ind= input.findIndex(e=>e==el)
+  
+  let insertInd = getInsertInd(ind,el)
+  
+  //remove self
   input.splice(ind,1)
-  input.splice(instertInd ,0,el)
+
+  //insert new
+  input.splice(insertInd ,0,el)
+  
+  //console.log(input.join(', '))
 })
 let indOf0 = input.findIndex((e)=>e==0)
 let sum = [
-  input[(indOf0+1000)%input.length],
-  input[(indOf0+2000)%input.length],
-  input[(indOf0+3000)%input.length],
+  input[(indOf0+1000)%(len)],
+  input[(indOf0+2000)%(len)],
+  input[(indOf0+3000)%(len)],
 ]
 // < 6954
 // < 20016
+//not 20603
 console.log('p1',arrSum(sum))
